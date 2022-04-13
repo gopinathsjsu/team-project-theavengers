aabe847ae5bc822aaa7fb203c085e33fd3c6c0fd
Teja Ramisetty <teja.ramisetty@sjsu.edu>
2022-04-13 16:42:48 -0700

    Created main tables
    Created new tables for our project

--- DataBase.sql
+++ DataBase.sql
@@ -1,1 +1,18 @@
 create database hotelBooking
+use hotelBooking;
+
+CREATE TABLE Users (
+    UserID int auto_increment,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Gender varchar(25),
+    Address varchar(255),
+    City varchar(255),
+    State varchar(255),
+    Country varchar(255),
+    ZipCode varchar(255),
+    emailID varchar(255),
+    pass varchar(255),
+    last_login datetime,
+    primary key(UserID)
+);
