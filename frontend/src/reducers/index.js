1c77d2473d5aefe66b90956538c0da0a2ab48ed4
Gali Saketh <sakethgali@gmail.com>
2022-05-04 20:50:12 -0700

    SignUp reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,3 +1,4 @@
 import { combineReducers } from 'redux';
+import customerLoginReducer from './loginReducer';
 
-export default combineReducers();
+export default combineReducers(customerLoginReducer);
