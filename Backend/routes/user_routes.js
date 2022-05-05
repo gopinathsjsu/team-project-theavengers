550c85f902ce6b06aa616ff4969eb1bbe7cbc542
Harshini Komali <harshinikomali73@gmail.com>
2022-05-05 15:59:30 -0700

    adding update user details route

--- Backend/routes/user_routes.js
+++ Backend/routes/user_routes.js
@@ -3,4 +3,5 @@ const controller = require("../controllers/user_controller");
 const router = express();
 router.post('/signin',controller.signIn);
 router.post('/signup',controller.signUp);
+router.post('/updatedetails',controller.updateProfile);
 module.exports = router;
