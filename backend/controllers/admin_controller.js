54f3bbcb80e9a84ed16e00eca961f79d0ff64359
Teja Ramisetty <teja.ramisetty@sjsu.edu>
2022-05-05 23:29:59 -0700

    updated addhotel fundtion

---
+++ backend/controllers/admin_controller.js
@@ -0,0 +1,27 @@
+const Hotel = require("../models/hotel_model");
+
+
+//adding new Hotel
+exports.addHotel=(req,res)=>{
+
+    if (req.body){
+        const hotel=new Hotel({
+            name:req.body.name,
+            star:req.body.star,
+            rooms:req.body.rooms,
+            address:req.body.address,
+            amenities:req.body.amenities,
+            city:req.body.city,
+            state:req.body.state,
+            country:req.body.country,
+            contact:req.body.contact
+        });
+
+        hotel.save((err)=>{
+            if(err){
+                res.status(500).send({message:err});
+            }
+            res.send({message:"Hotel added successfully"});
+        });
+    }
+};
