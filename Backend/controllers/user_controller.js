a3f8ba612c477e33b02abdfc7912138e24bd991e
Teja Ramisetty <teja.ramisetty@sjsu.edu>
2022-05-13 18:21:38 -0700

    updated user controller and server.js

--- Backend/controllers/user_controller.js
+++ Backend/controllers/user_controller.js
@@ -1,7 +1,11 @@
-const db = require("../models");
-const User = db.user;
+const User = require("../models/user_model");
+const Booking = require("../models/booking_model");
+const Room=require("../models/rooms_model");
+const date = require('date-and-time');
 var bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
 
+//User Sign Up
 exports.signUp=(req,res)=>{
     User.findOne({
         email:req.body.email
@@ -32,7 +36,7 @@ exports.signUp=(req,res)=>{
         }
     });
 };
-
+//User SignIn
 exports.signIn=(req,res)=>{
     User.findOne({email:req.body.email})
     .exec((err,user)=>{
@@ -58,6 +62,7 @@ exports.signIn=(req,res)=>{
         });
     });
 };
+//User profile update
 exports.updateProfile = (req,res) => {
 	User.findOne({
 		email:req.body.email
@@ -93,6 +98,95 @@ exports.updateProfile = (req,res) => {
 				res.send({message:"Details updated successfully"});
 			});
 		});
-		
-
 };
+//Price Details
+exports.viewPrice=(req,res)=>{
+    Room.findOne({
+        hotel_id:req.body.hotel_id,
+        roomType:req.body.roomType
+    })
+    .exec((err,hotelDetails)=>{
+        if(err){
+            res.status(500).send({message:err});
+        }
+        var date1 =new Date(req.body.checkInDate);
+        var date2 =new Date(req.body.checkOutDate);
+        const days=parseInt((date2-date1)/(1000 * 60 * 60 * 24));
+        const price=(hotelDetails.basePrice)*days
+        const checkInMonth=(date1.getMonth())+1;
+        const checkOutMonth=(date2.getMonth())+1;
+        //Thanks giving Christmas pricing
+        if((date1.getDate()+1>19 && checkInMonth==11)||checkInMonth==12){
+            price+=(price*0.20);
+        }
+        //Summer Season pricing
+        else if((checkInMonth==4||checkInMonth==5)||(checkOutMonth==4||checkOutMonth==5)){
+            price+=(price*0.20);
+        }
+        //Weekend Prcing
+        else if (date1.getDay()>date2.getDay()||date2.getDay()==7){
+            price+=(price*0.15);
+        }
+    });
+ };
+ //Redeem reward points
+ exports.redeemRewards=(req,res)=>{
+     User.findOne({
+         _id:mongoose.Types.ObjectId(req.body._id)
+     })
+     .exec((err,result)=>{
+       if(err){
+           res.status(500).send({message:err});
+       }  
+       if (result.rewardPoints>0){
+            var price=req.body.price
+           if (result.rewardPoints<req.body.price){
+            price-=result.rewardPoints;
+            User.findOneAndUpdate({_id:mongoose.Types.ObjectId(req.body._id)},{rewardPoints:0}).exec((error,results)=>{
+                if (error){
+                    res.status(500).send({message:err});
+                }
+            })
+           }
+           else{
+               User.findOneAndUpdate({_id:mongoose.Types.ObjectId(req.body._id)},{rewardPoints:result.rewardPoints-price})
+               .exec((error,results)=>{
+                   if(error){
+                       res.status(500).send({message:err});
+                   }
+               })
+               price=0
+           }
+           res.send({price})
+       }
+       else{
+           res.send({message:"You dont have any reward points"})
+       }
+     });
+ };
+//User Booking
+exports.userBooking=(req,res)=>{
+    if(req.body){
+        const booking=new Booking({
+            userId:req.body.userId,
+            hotelId:req.body.hotelId,
+            hotelName:req.body.name,
+            checkInDate:req.body.checkInDate,
+            checkOutDate:req.body.checkOutDate,
+            roomType:req.body.roomType,
+            guestList:req.body.guestList,
+            amountPaid:req.body.amountPaid,
+            bookingStatus:req.body.bookingStatus,
+            bookingDate:Date.now()
+        });
+        booking.save((err)=>{
+            if (err){
+                res.status(500).send({message:err});
+            }
+            res.send({message:"Successfully booked"});
+        }
+
+        );
+    }
+}
+
